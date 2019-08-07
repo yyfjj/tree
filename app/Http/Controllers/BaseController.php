1e98051640f8d004e59a35e0f62dad663dab662c
yyfjj <yyfjj@foxmail.com>
2019-08-07 10:03:08 +0800

    基类

---
+++ app/Http/Controllers/BaseController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers;
+
+//use Illuminate\Http\Request;
+use Dingo\Api\Routing\Helpers;
+use Illuminate\Routing\Controller;
+
+class BaseController extends Controller
+{
+    use Helpers;
+}
